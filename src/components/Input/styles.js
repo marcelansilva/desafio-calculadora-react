4e513125ce85c337f0faa1c45695935a965df02e
Marcela Nascimento Silva <marcelamns.dev@gmail.com>
2022-12-05 21:42:26 -0300

    first commit

---
+++ src/components/Input/styles.js
@@ -0,0 +1,25 @@
+import styled from "styled-components";
+
+export const InputContainer = styled.div`
+  width: 100%;
+  height: 75px;
+  background-color: #000000;
+
+  display: flex;
+  align-items: center;
+  justify-content: flex-end;
+  font-size: 24px;
+
+  input {
+    width: 100%;
+    height: 75px;
+    background-color: #000000;
+    border: 0;
+    display: flex;
+    flex-direction: column;
+    align-items: flex-end;
+    padding: 0 10px;
+    font-size: 24px;
+    color: #ffffff;
+  }
+`;
